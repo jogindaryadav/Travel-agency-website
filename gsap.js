f258c560fef82dc0182aa5c0e2c7ee77672b2200
jogindaryadav <jogindary426@gmail.com>
2025-02-04 21:40:47 +0545

    addinggsap.js

---
+++ gsap.js
@@ -0,0 +1,37 @@
+// gsap.registerPlugin(ScrollTrigger);
+// gsap.to(".cards", {duration: 4,
+//     scale: 1.5,
+//     // rotationY: 45,
+//     x: 10,
+//     y: 0,
+//     z: -200 ,
+//     scrollTrigger: {
+//         trigger: ".cards",
+//         start: "400 center", // When the element reaches the center of the viewport
+//         end: "800 top", // When the element leaves the viewport
+//         scrub: true,
+//         markers:true, // Smooth transition while scrolling
+//     }});
+
+//     gsap.to(".className",{
+//         //property 
+//         translateX:
+//         scrollTrigger:{
+//             trigger:".className",
+//         }
+
+//     })
+
+//     gsap.to(".bgHero",{ 
+//         opacity:0.9,
+//         duration:5,
+//         scrollTrigger: {
+//             trigger: ".bgHero",
+//             start: "400 center", // When the element reaches the center of the viewport
+//             end: "300 top", // When the element leaves the viewport
+//             scrub: true,
+//             markers:true, // Smooth transition while scrolling
+//         }
+//     });
+
+  
