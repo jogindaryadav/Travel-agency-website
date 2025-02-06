cb09ea37eb0c6bb4c3d949a3769002292fbdd3e6
jogindaryadav <jogindary426@gmail.com>
2025-02-06 17:48:00 +0545

    slider

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-gsap.from("")
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 4,
+      margin: 10,
+      loop: true,
+      nav: true
+    });
+  });
